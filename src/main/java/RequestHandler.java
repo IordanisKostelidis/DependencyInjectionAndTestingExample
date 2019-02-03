3cfec4588363c3d3bf06c888837a17573ec157a1
Iordanis Kostelidis <iordkost@teicm.gr>
2019-02-03 22:46:21 +0200

    Dependency Injection Refactor

--- src/main/java/RequestHandler.java
+++ src/main/java/RequestHandler.java
@@ -5,12 +5,9 @@ public class RequestHandler {
 
     static Instant dateOfLastRequest = null;
 
-    void handleNotMoreOneRequestPerDay(ARequest request) throws TooManyRequestsException {
-        // The problem, if we want to test a request the next day, we must wait 24 hours
-        // We must fix this with Dependency Injection methodology
-        // (pass the dependency from Constructor on Class or as Parameter on method)
-        Instant now = Instant.now();
-
+    void handleNotMoreOneRequestPerDay(ARequest request, Instant now) throws TooManyRequestsException {
+        // Now we get the current date (Instant now) as Parameter
+        // Instant now = Instant.now();
 
         if (dateOfLastRequest != null) {
             Instant previousDate = now.minus(24, ChronoUnit.HOURS);
