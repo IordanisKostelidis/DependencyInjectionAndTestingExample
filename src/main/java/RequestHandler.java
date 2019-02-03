c5927e01ee6a5f28843e486476c599b57a0f473a
Iordanis Kostelidis <iordkost@teicm.gr>
2019-02-03 22:41:34 +0200

    Fix Typos

--- src/main/java/RequestHandler.java
+++ src/main/java/RequestHandler.java
@@ -2,20 +2,29 @@ import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
 public class RequestHandler {
+
     static Instant dateOfLastRequest = null;
-    void handleNotMoreOneRequestPerDay(ARequest request) throws
-            TooManyRequestsException();
-    {
+
+    void handleNotMoreOneRequestPerDay(ARequest request) throws TooManyRequestsException {
         Instant now = Instant.now();
 
         if (dateOfLastRequest != null) {
             Instant previousDate = now.minus(24, ChronoUnit.HOURS);
+
             if (previousDate.isBefore(dateOfLastRequest)) {
-                throw TooManyRequestsException();
+                throw new TooManyRequestsException();
             }
         }
 
         dateOfLastRequest = now;
         // handle request ...
     }
+}
+
+class ARequest {
+    // A Request class
+}
+
+class TooManyRequestsException extends Exception {
+    // Our Custom Exception
 }
