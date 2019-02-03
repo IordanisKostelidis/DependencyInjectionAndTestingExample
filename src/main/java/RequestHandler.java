31accd05590f57fd69e44d5cf859213646da7588
Iordanis Kostelidis <iordkost@teicm.gr>
2019-02-03 22:44:36 +0200

    Problem Found

--- src/main/java/RequestHandler.java
+++ src/main/java/RequestHandler.java
@@ -6,7 +6,11 @@ public class RequestHandler {
     static Instant dateOfLastRequest = null;
 
     void handleNotMoreOneRequestPerDay(ARequest request) throws TooManyRequestsException {
+        // The problem, if we want to test a request the next day, we must wait 24 hours
+        // We must fix this with Dependency Injection methodology
+        // (pass the dependency from Constructor on Class or as Parameter on method)
         Instant now = Instant.now();
+
 
         if (dateOfLastRequest != null) {
             Instant previousDate = now.minus(24, ChronoUnit.HOURS);
